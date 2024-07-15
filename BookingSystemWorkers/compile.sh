25bbe6591e5744fb35d76474176d16bffc97c3ad
philipz <philipzheng@gmail.com>
2024-07-15 23:49:50 +0800

    feat: Multi-Arch builder and Add async in BPMN

--- BookingSystemWorkers/compile.sh
+++ BookingSystemWorkers/compile.sh
@@ -3,7 +3,7 @@ cd /app
 #apk add upx
 npm install
 npm install -g pkg
-pkg --loglevel info -t node16-linuxstatic -o ./$2 ./$1
+pkg --loglevel info -t node18-linuxstatic -o ./$2 ./$1
 #upx --best -k $2
 #rm $2.~
 mkdir build && mv $2 build
